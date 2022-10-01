5ba1362340ac77fc100c1937c8b7b4ae69f2bcc7
Fabio <fff>
2022-10-01 11:47:05 +0100

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Fábio Pontes on 01/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
